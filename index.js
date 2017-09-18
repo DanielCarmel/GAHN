dd98dd88b91f8ac29adfc06bb3066069ca612a99
DANIELCARMEL <danielcarmel6@gmail.com>
2017-09-18 23:06:13 +0300

    only need to check basically

--- index.js
+++ index.js
@@ -20,15 +20,31 @@ server.listen(port, function(){
 
 // Temp post method
 app.post('/', function(req, res){
-    arr = JSON.parse(req.body.data);
+    var template = '';
+    var arr = JSON.parse(req.body.data);
     for(var key in arr){
-        console.log(arr[key].url)
+        var currDate = new Date(Number(arr[key].time));
+        template += '<tr><td class="lalign">'+ arr[key].url +'</td><td>'+ currDate +'</td></tr>'
     }
-});
 
-app.get("/", function(req, res){
-    res.send("hello")
-})
+    // mail variable
+    var mailOptions = {
+        from: 'Daniel <danielcarmel6@gmail.com>',
+        to: 'daniel.store.ebay@gmail.com',
+        subject: 'The shit works!',
+        text: 'Hey future bae',
+        html: setTemplate(template)
+    };
+
+    // Send the mail
+    transporter.sendMail(mailOptions, function(err, res){
+        if(err){
+            console.log(err);
+        } else {
+            console.log('Email sent');
+        }
+    })
+});
 
 // Info about Gmail api
 var transporter = nodemailer.createTransport({
@@ -43,20 +59,171 @@ var transporter = nodemailer.createTransport({
     }
 })
 
-// mail variable
-var mailOptions = {
-    from: 'Daniel <danielcarmel6@gmail.com>',
-    to: 'daniel.store.ebay@gmail.com',
-    subject: 'The shit works!',
-    text: 'Hey future bae',
-    html: '<p>HTML version of message(recommended)</p>'
-};
+function setTemplate(templateData){
+    var templateFull = '<html>\
+    <head>\
+        <script>\
+            $(function () {\
+                $("#keywords").tablesorter();\
+            });\
+        </script>\
+        <style>\
+            @import url("https://fonts.googleapis.com/css?family=Amarante");\
+            html,body,div,span,applet,object,iframe,h2,h3,h4,h5,h6,p,blockquote,pre,a,abbr,acronym,address,\
+            big,cite,code,del,dfn,em,img,ins,kbd,q,s,samp,small,strike,strong,sub,sup,tt,var,b,u,i,center,dl,\
+            dt,dd,ol,ul,li,fieldset,form,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,article,aside,\
+            canvas,details,embed,figure,figcaption,footer,header,hgroup,menu,nav,output,ruby,section,summary,\
+            time,mark,audio,\
+            video {\
+                margin: 0;\
+                padding: 0;\
+                border: 0;\
+                font-size: 100%;\
+                font: inherit;\
+                vertical-align: baseline;\
+                outline: none;\
+                -webkit-font-smoothing: antialiased;\
+                -webkit-text-size-adjust: 100%;\
+                -ms-text-size-adjust: 100%;\
+                -webkit-box-sizing: border-box;\
+                -moz-box-sizing: border-box;\
+                box-sizing: border-box;\
+            }\
+            html {\
+                overflow-y: scroll;\
+            }\
+            body {\
+                background: #eee url("https://i.imgur.com/eeQeRmk.png");\
+                /* https://subtlepatterns.com/weave/ */\
+                font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;\
+                font-size: 62.5%;\
+                line-height: 1;\
+                color: #585858;\
+                padding: 22px 10px;\
+                padding-bottom: 55px;\
+            }\
+            ::selection {\
+                background: #5f74a0;\
+                color: #fff;\
+            }\
+            ::-moz-selection {\
+                background: #5f74a0;\
+                color: #fff;\
+            }\
+            ::-webkit-selection {\
+                background: #5f74a0;\
+                color: #fff;\
+            }\
+            br {\
+                display: block;\
+                line-height: 1.6em;\
+            }\
+            article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,\
+            section {\
+                display: block;\
+            }\
+            ol,\
+            ul {\
+                list-style: none;\
+            }\
+            input,\
+            textarea {\
+                -webkit-font-smoothing: antialiased;\
+                -webkit-text-size-adjust: 100%;\
+                -ms-text-size-adjust: 100%;\
+                -webkit-box-sizing: border-box;\
+                -moz-box-sizing: border-box;\
+                box-sizing: border-box;\
+                outline: none;\
+            }\
+            blockquote,\
+            q {\
+                quotes: none;\
+            }\
+            blockquote:before,\
+            blockquote:after,\
+            q:before,\
+            q:after {\
+                content: "";\
+                content: none;\
+            }\
+            strong,\
+            b {\
+                font-weight: bold;\
+            }\
+            table {\
+                border-collapse: collapse;\
+                border-spacing: 0;\
+            }\
+            img {\
+                border: 0;\
+                max-width: 100%;\
+            }\
+            /** page structure **/\
+            #wrapper {\
+                display: block;\
+                width: 850px;\
+                background: #fff;\
+                margin: 0 auto;\
+                padding: 10px 17px;\
+                -webkit-box-shadow: 2px 2px 3px -1px rgba(0, 0, 0, 0.35);\
+            }\
+            #keywords {\
+                margin: 0 auto;\
+                font-size: 1.2em;\
+                margin-bottom: 15px;\
+            }\
+            #keywords thead {\
+                cursor: pointer;\
+                background: #c9dff0;\
+            }\
+            #keywords thead tr th {\
+                font-weight: bold;\
+                padding: 12px 30px;\
+                padding-left: 42px;\
+            }\
+            #keywords thead tr th span {\
+                padding-right: 20px;\
+                background-repeat: no-repeat;\
+                background-position: 100% 100%;\
+            }\
+            #keywords thead tr th.headerSortUp,\
+            #keywords thead tr th.headerSortDown {\
+                background: #acc8dd;\
+            }\
+            #keywords thead tr th.headerSortUp span {\
+                background-image: url("https://i.imgur.com/SP99ZPJ.png");\
+            }\
+            #keywords thead tr th.headerSortDown span {\
+                background-image: url("https://i.imgur.com/RkA9MBo.png");\
+            }\
+            #keywords tbody tr {\
+                color: #555;\
+            }\
+            #keywords tbody tr td {\
+                text-align: center;\
+                padding: 15px 10px;\
+            }\
+            #keywords tbody tr td.lalign {\
+                text-align: left;\
+            }\
+        </style>\
+    </head>\
+    <body>\
+        <div id="wrapper">\
+            <table id="keywords" cellspacing="0" cellpadding="0">\
+                <thead>\
+                    <tr>\
+                        <th><span>URL</span></th>\
+                        <th><span>Time</span></th>\
+                    </tr>\
+                </thead>\
+                <tbody>' + templateData + '\
+                </tbody>\
+            </table>\
+        </div>\
+    </body>\
+    </html>'
 
-// Send the mail
-transporter.sendMail(mailOptions, function(err, res){
-    if(err){
-        console.log(err);
-    } else {
-        console.log('Email sent');
-    }
-})
+    return(templateFull)
+}
